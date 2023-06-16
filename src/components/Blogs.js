aea419ac62a0741922b379e0acb5e53d16ba5a73
babsmutini <scotteyawo@gmail.com>
2023-06-16 15:28:57 +0100

    update

---
+++ src/components/Blogs.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import { formatDate } from "../utilities/helper";
+const Blog = ({ author, title, content, date, lastUpdated, id, audio }) => {
+    const navigate = useNavigate();
+    return (
+        <div
+            className="cursor-pointer flex text-left flex-col gap-y-2 bg-[#f0f3f4] p-4 rounded-md"
+            onClick={() => navigate(`/blog/${id}`, { state: { title, content, audio } })}
+        >
+            <div className="flex justify-between">
+                <p className="font-semibold">{author || "Adewale adekanye"}</p>
+                <p>{formatDate(lastUpdated)}</p>
+            </div>
+            <p className="font-semibold line-clamp-1 text-lg">{title || "Health sector and matters"}</p>
+            <p className="line-clamp-2">
+                {content ||
+                    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem provident deserunt, eaque nam assumenda nisi officiis velit corporis? Possimus placeat hic accusantium velit corporis cupiditate consectetur vero nulla dolore officiis."}
+            </p>
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+	<div className='bg-white p-4 shadow-sm px-16'>
+		<h1 className='mx-auto text-2xl font-semibold text-left'> Speechify.</h1>
+	</div>
+  )
+}
+
+export default Header
